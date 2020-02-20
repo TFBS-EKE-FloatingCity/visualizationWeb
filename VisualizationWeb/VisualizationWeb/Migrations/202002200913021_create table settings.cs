0552e69efb904fce48228f2934747ae89aba5eaa
gertbirkner@hotmail.com <gertbirkner@hotmail.com>
2020-02-20 10:13:23 +0100

    create table settings

---
+++ VisualizationWeb/VisualizationWeb/Migrations/202002200913021_create table settings.cs
@@ -0,0 +1,31 @@
+namespace VisualizationWeb.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class createtablesettings : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Settings",
+                c => new
+                    {
+                        SettingID = c.Int(nullable: false, identity: true),
+                        WindMax = c.Double(nullable: false),
+                        SunMax = c.Double(nullable: false),
+                        ConsumptionMax = c.Double(nullable: false),
+                        WindActive = c.Boolean(nullable: false),
+                        SunActive = c.Boolean(nullable: false),
+                        ConsumptionActive = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.SettingID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Settings");
+        }
+    }
+}
