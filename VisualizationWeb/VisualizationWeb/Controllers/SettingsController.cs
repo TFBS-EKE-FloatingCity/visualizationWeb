0c74bb03a22517eec5308b5e6c6014c8f309a65a
Patrick Eccher <LocalUser@VM-INFORMATIK>
2020-02-26 16:27:26 +0100

    add settingsvm, changes in controller and views

--- VisualizationWeb/VisualizationWeb/Controllers/SettingsController.cs
+++ VisualizationWeb/VisualizationWeb/Controllers/SettingsController.cs
@@ -4,21 +4,24 @@ using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using VisualizationWeb.Helpers;
 using VisualizationWeb.Models;
+using VisualizationWeb.Models.ViewModel;
 
 namespace VisualizationWeb.Controllers
 {
     public class SettingsController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
+        SettingVM settingVM = new SettingVM();
 
         // GET: Settings
         public ActionResult Index()
         {
             Setting setting = db.Settings.FirstOrDefault();
-
             if (setting == null)
             {
                 setting = new Setting
@@ -32,7 +35,15 @@ namespace VisualizationWeb.Controllers
                 };
             }
 
-            return View(setting);
+            settingVM.SettingID = setting.SettingID;
+            settingVM.WindMax = UnitCalc.NumberToPrefix(setting.WindMax);
+            settingVM.SunMax = UnitCalc.NumberToPrefix(setting.SunMax);
+            settingVM.ConsumptionMax = UnitCalc.NumberToPrefix(setting.ConsumptionMax);
+            settingVM.SunActive = setting.SunActive;
+            settingVM.WindActive = setting.WindActive;
+            settingVM.ConsumptionActive = setting.ConsumptionActive;
+
+            return View(settingVM);
         }
 
         // GET: Settings/Edit/5
@@ -42,34 +53,25 @@ namespace VisualizationWeb.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Setting setting = db.Settings.FirstOrDefault();
-            if (setting == null)
-            {
-                setting = new Setting
-                {
-                    SunActive = true,
-                    WindActive = true,
-                    ConsumptionActive = true,
-                    SunMax = 10000000.0,
-                    WindMax = 100000.0,
-                    ConsumptionMax = 5000000.0
-                };
-            }
-            return View(setting);
+            return View(settingVM);
         }
 
         // POST: Settings/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "SettingID,WindMax,SunMax,ConsumptionMax,WindActive,SunActive,ConsumptionActive")] Setting setting)
+        public async Task<ActionResult> Edit([Bind(Include = "SettingID,WindMax,SunMax,ConsumptionMax,WindActive,SunActive,ConsumptionActive")] SettingVM settingVM)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(setting).State = EntityState.Modified;
-                db.SaveChanges();
+                Setting set = db.Settings.Find(settingVM.SettingID);
+                set.WindMax = UnitCalc.PrefixToNumber(settingVM.WindMax);
+                set.SunMax = UnitCalc.PrefixToNumber(settingVM.SunMax);
+                set.ConsumptionMax = UnitCalc.PrefixToNumber(settingVM.ConsumptionMax);
+
+                await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
-            return View(setting);
+            return View(settingVM);
         }
 
         protected override void Dispose(bool disposing)
