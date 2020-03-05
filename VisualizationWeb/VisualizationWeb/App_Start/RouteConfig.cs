b39ddf941c6e44645ece4e40e52fe0498e1a21e6
Rhomberg Alessandro <LocalUser@VM-INFORMATIK>
2020-03-05 11:15:24 +0100

    edit styles

--- VisualizationWeb/VisualizationWeb/App_Start/RouteConfig.cs
+++ VisualizationWeb/VisualizationWeb/App_Start/RouteConfig.cs
@@ -15,7 +15,7 @@ namespace VisualizationWeb
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Dashboard", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
