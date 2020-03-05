61d53bd5584398fce6c3ba092199cc3506a74e66
Patrick Eccher <LocalUser@VM-INFORMATIK>
2020-03-05 10:23:48 +0100

    simtypes index delete edit button, chontroller edit redirect to index

--- VisualizationWeb/VisualizationWeb/Controllers/SimTypesController.cs
+++ VisualizationWeb/VisualizationWeb/Controllers/SimTypesController.cs
@@ -116,7 +116,7 @@ namespace VisualizationWeb.Controllers
 
                 db.Entry(simType).State = EntityState.Modified;
                 await db.SaveChangesAsync();
-                return RedirectToAction("Details", simType.SimTypeID);
+                return RedirectToAction("Index");
             }
             return View(simType);
         }
