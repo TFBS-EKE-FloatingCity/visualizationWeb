5c1135c89632dae5ebbd00a43c1c6e537d6f7511
FloatingCity Simulation <floatingsimcity@outlook.com>
2020-11-25 15:35:54 +0100

    SimulationController zu VisualisationWeb hinzugefügt
    Verknüpfte Arbeitselemente: #300

---
+++ VisualizationWeb/VisualizationWeb/Controllers/SimulationsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VisualizationWeb.Controllers
+{
+    public class SimulationsController : Controller
+    {
+        // GET: Simulations
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
