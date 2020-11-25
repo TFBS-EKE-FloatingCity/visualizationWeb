c09fee6968b5d47eb8b3cf51746da2d2e4c2cb08
FloatingCity Simulation <floatingsimcity@outlook.com>
2020-11-25 15:06:31 +0100

    SimPosition und SimScenario Models hinzugefügt
    Verknüpfte Arbeitselemente: #300

---
+++ VisualizationWeb/Simulation.Library/Models/SimPosition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation.Library.Models
+{
+    public class SimPosition
+    {
+        [Key]
+        public int SimPositionID { get; set; }
+
+        [Required]
+        public int SunValue { get; set; }
+
+        [Required]
+        public int WindValue { get; set; }
+
+        [Required]
+        public int EnergyBalanceValue { get; set; }
+
+        [Required]
+        public DateTime DateRegistered { get; set; }
+
+        public int SimScenarioID { get; set; }
+
+        public SimScenario SimScenario { get; set; }
+    }
+}
