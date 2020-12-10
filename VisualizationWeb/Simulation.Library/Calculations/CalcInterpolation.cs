2595742b0b12c2e877c47d49f94a8651287a53e4
FloatingCity Simulation <floatingsimcity@outlook.com>
2020-12-10 15:38:59 +0100

    fixed typo

---
+++ VisualizationWeb/Simulation.Library/Calculations/CalcInterpolation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation.Library.Calculations
+{
+    public static class CalcInterpolation
+    {
+        //Returns searched Value between 2 Function positions
+        public static int GetValue(int startY, decimal startX, int endX, decimal endY, decimal xGiven)
+        {
+            return (int)Math.Round(startY + ((endX - startY) / (endY - startX)) * (xGiven - startX));
+        }
+    }
+}
