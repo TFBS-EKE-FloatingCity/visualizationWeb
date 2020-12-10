57028c8a83154591de0a1de9a7ba958a60f441e9
FloatingCity Simulation <floatingsimcity@outlook.com>
2020-12-10 16:26:06 +0100

    Sim Create View angepasst
    Verknüpfte Arbeitselemente: #300

--- VisualizationWeb/Simulation.Library/Calculations/calcInterpolation.cs
+++ VisualizationWeb/Simulation.Library/Calculations/calcInterpolation.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Simulation.Library.Calculations
 {
-    public static class calcInterpolation
+    public static class CalcInterpolation
     {
         //Returns searched Value between 2 Function positions
         public static int GetValue(int startY, decimal startX, int endX, decimal endY, decimal xGiven)
