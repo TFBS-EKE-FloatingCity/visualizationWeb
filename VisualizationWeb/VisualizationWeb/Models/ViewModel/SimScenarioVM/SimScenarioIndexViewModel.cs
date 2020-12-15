7b22371268da01d1c07d3c035a9c463d8e6627e9
FloatingCity Simulation <floatingsimcity@outlook.com>
2020-12-15 16:36:56 +0100

    ViewModels erstellt
    Verknüpfte Arbeitselemente: #300

---
+++ VisualizationWeb/VisualizationWeb/Models/ViewModel/SimScenarioVM/SimScenarioIndexViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VisualizationWeb.Models.ViewModel.SimScenarioVM
+{
+    public class SimScenarioIndexViewModel
+    {
+        [Key]
+        public int SimScenarioID { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 1)]
+        public string Title { get; set; }
+
+        public int TimeFactor { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+    }
+}
