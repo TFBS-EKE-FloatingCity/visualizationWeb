7f2953b49c137ef4216b71887937974f114087b2
Stephanie Fritz <stefritz@eke.at>
2021-01-07 20:17:49 +0100

    Get new Databasemodel and websocket settings  - copied from feature/socket_io_models 07012021

---
+++ VisualizationWeb/VisualizationWeb/Helpers/Temporary/SimulationService.cs
@@ -0,0 +1,62 @@
+﻿using Simulation.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VisualizationWeb.Helpers.Temporary {
+    public class SimulationService : ISimulationService {
+        public int SimulationScenarioId { get; set; }
+
+        public int MaxEnergyProductionWind { get; set; }
+
+        public int MaxEnergyProductionSun { get; set; }
+
+        public int MaxEnergyConsumption { get; set; }
+
+        public bool IsSimulationRunning { get; set; }
+
+        public DateTime? StartDateTimeReal { get; set; }
+
+        public decimal TimeFactor { get; set; }
+
+        public event EventHandler SimulationStarted;
+        public event EventHandler SimulationEnded;
+
+        public void Dispose() {
+            throw new NotImplementedException();
+        }
+
+        public int? GetEnergyBalance(DateTime timeStamp) {
+            return null;
+        }
+
+        public int? GetEnergyConsumption(DateTime timeStamp) {
+            return null;
+        }
+
+        public int? GetEnergyProductionSun(DateTime timeStamp) {
+            return null;
+        }
+
+        public int? GetEnergyProductionWind(DateTime timeStamp) {
+            return null;
+        }
+
+        public DateTime? GetSimulatedTimeStamp(DateTime timeStamp) {
+            return null;
+        }
+
+        public void Run() {
+            throw new NotImplementedException();
+        }
+
+        //public void SetSimulationScenario(SimScenario scenario) {
+        //    throw new NotImplementedException();
+        //}
+
+        public void Stop() {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VisualizationWeb/VisualizationWeb/Helpers/WebsocketServerConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace VisualizationWeb.Helpers {
+    public class WebsocketServerConfiguration : WebSocketBehavior {
+        protected override void OnOpen() {
+            base.OnOpen();
+        }
+
+        protected override void OnClose(CloseEventArgs e) {
+            base.OnClose(e);
+        }
+
+        protected override void OnMessage(MessageEventArgs e) {
+            base.OnMessage(e);
+        }
+
+        protected override void OnError(ErrorEventArgs e) {
+            base.OnError(e);
+        }
+    }
+}
